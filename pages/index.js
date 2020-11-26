f8a2d3b81ac81021b413e6713b94a1ffeecdb453
_haochuan <haochuan.liu@gmail.com>
2020-11-25 22:27:41 -0800

    site title and ico

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import styles from '../styles/Home.module.css';
 import Messages from '../components/Messages';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Chat with Haochuan</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <main className={styles.main}>
         <Messages />
       </main>
